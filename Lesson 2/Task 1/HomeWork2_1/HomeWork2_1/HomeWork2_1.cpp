9e8e638386158d42ef4925a56d533d70fe28671b
Baryshkov Mikhail <m.baryshkov@mail.ru>
2023-12-18 21:45:44 +0300

    Inition commit

---
+++ Lesson 2/Task 1/HomeWork2_1/HomeWork2_1/HomeWork2_1.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+
+enum class months : short {
+    Jan = 1,
+    Feb,
+    Marth,
+    April,
+    May,
+    June,
+    Jule,
+    Aug,
+    Sep,
+    Oct,
+    Nov,
+    Dec
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+        short n;
+        do {
+            std::cout << "введите номер месяца: ";
+            std::cin >> n;
+            if (n > 12 || n < 0) {
+                std::cout << "Неправильный номер!" << std::endl;
+            }
+            else {
+                switch (n) {
+                case static_cast<int>(months::Jan):
+                    std::cout << "Январь" << std::endl;
+                    break;
+                case static_cast<int>(months::Feb):
+                    std::cout << "Февраль" << std::endl;
+                    break;
+                case static_cast<int>(months::Marth):
+                    std::cout << "Март" << std::endl;
+                    break;
+                case static_cast<int>(months::April):
+                    std::cout << "Апрель" << std::endl;
+                    break;
+                case static_cast<int>(months::May):
+                    std::cout << "Май" << std::endl;
+                    break;
+                case static_cast<int>(months::June):
+                    std::cout << "Июнь" << std::endl;
+                    break;
+                case static_cast<int>(months::Jule):
+                    std::cout << "Июль" << std::endl;
+                    break;
+                case static_cast<int>(months::Aug):
+                    std::cout << "Август" << std::endl;
+                    break;
+                case static_cast<int>(months::Sep):
+                    std::cout << "Сентябрь" << std::endl;
+                    break;
+                case static_cast<int>(months::Oct):
+                    std::cout << "Октябрь" << std::endl;
+                    break;
+                case static_cast<int>(months::Nov):
+                    std::cout << "Ноябрь" << std::endl;
+                    break;
+                case static_cast<int>(months::Dec):
+                    std::cout << "Декабрь" << std::endl;
+                    break;
+                }
+                }
+            } while (n != 0);       
+    if (n == 0) {
+        std::cout << "До свидания!";
+       
+    }
+    return 0;
+}
---
+++ Lesson 2/Task 2/HomeWork2_2/HomeWork2_2/HomeWork2_2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include<windows.h>
+
+struct person {
+        unsigned int nomer = 0;
+        std::string name;
+        double Balance = 0;
+        double NewBalance =0;
+    };
+
+void changeBalance(person& pers) {
+    pers.Balance = pers.NewBalance;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    person person;
+    std::cout << "Ввидите номер счета: ";
+    std::cin >> person.nomer;
+    std::cout << "Введите имя владельца : ";
+    std::cin >> person.name;
+    std::cout << "Введите баланс: ";
+    std::cin >> person.Balance;
+    std::cout << "Введите новый баланс: ";
+    std::cin >> person.NewBalance;
+    changeBalance(person);
+    std::cout << "Ваш счёт: " << person.name << ", " << person.nomer << ", " << person.Balance;
+}
+
