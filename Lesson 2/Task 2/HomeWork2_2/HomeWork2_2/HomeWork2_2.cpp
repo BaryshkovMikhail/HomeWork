518629b5d7ce0b8e5441b8d9a228ac1f93355db9
Baryshkov Mikhail <m.baryshkov@mail.ru>
2023-12-21 21:18:53 +0300

    Исправление в задаче 2

--- Lesson 2/Task 2/HomeWork2_2/HomeWork2_2/HomeWork2_2.cpp
+++ Lesson 2/Task 2/HomeWork2_2/HomeWork2_2/HomeWork2_2.cpp
@@ -5,11 +5,11 @@ struct person {
         unsigned int nomer = 0;
         std::string name;
         double Balance = 0;
-        double NewBalance =0;
+       
     };
 
-void changeBalance(person& pers) {
-    pers.Balance = pers.NewBalance;
+void changeBalance(person& pers, int& NewBalance) {
+    pers.Balance = NewBalance;
 }
 
 int main()
@@ -17,6 +17,7 @@ int main()
     setlocale(LC_ALL, "Russian");
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
+    int NewBalance;
     person person;
     std::cout << "Ввидите номер счета: ";
     std::cin >> person.nomer;
@@ -25,8 +26,8 @@ int main()
     std::cout << "Введите баланс: ";
     std::cin >> person.Balance;
     std::cout << "Введите новый баланс: ";
-    std::cin >> person.NewBalance;
-    changeBalance(person);
+    std::cin >> NewBalance;
+    changeBalance(person, NewBalance);
     std::cout << "Ваш счёт: " << person.name << ", " << person.nomer << ", " << person.Balance;
 
     return 0;
