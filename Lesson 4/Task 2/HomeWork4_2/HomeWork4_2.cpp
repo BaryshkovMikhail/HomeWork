f5330dda25c279e8fcf31d6ab3ab1119965033f0
Baryshkov Mikhail <m.baryshkov@mail.ru>
2024-01-04 17:31:58 +0300

    Закончил задание 4-2

--- Lesson 4/Task 2/HomeWork4_2/HomeWork4_2.cpp
+++ Lesson 4/Task 2/HomeWork4_2/HomeWork4_2.cpp
@@ -1,20 +1,103 @@
-﻿// HomeWork4_2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <windows.h>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <sstream>
 
-#include <iostream>
+class Adress
+{
+private:
+    std::string city;
+    std::string street;
+    unsigned int house;
+    unsigned int kvartira;
+public:
+
+    std::string get_output_address() {
+        std::ostringstream new_adress;
+        new_adress << city << ", " << street << ", " << house << ", " << kvartira;
+        return new_adress.str();
+    }
+
+    Adress(std::string city, std::string street, unsigned int house, unsigned int kvartira)
+        :city(city), street(street), house(house), kvartira(kvartira) {}
+
+};
+
+
+void sort_adress(std::string* arr, int size)
+{
+    std::string tmp;
+    for (int i = 0; i < size - 1; i++){
+        for (int j = 0; j < size - 1; ++j){
+            if (arr[j + 1] < arr[j])
+            {
+                tmp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = tmp;
+            }
+        }
+}
+
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    std::ifstream file("in.txt");
+    int rows;
+    std::string arr[5];
+    std::string city;
+    std::string street;
+    unsigned int house;
+    unsigned int kvartira;
+
+
+    if (file.is_open())
+    {
+        bool fileIsOver;
+        fileIsOver = file.eof();
+        file >> rows;
+        std::ofstream outFile("out.txt");
+        if (outFile.is_open()) {
+            outFile << rows << std::endl;
+        }
+        else {
+            std::cout << "Не получилось открыть файл!" << std::endl;
+        }
+        while (!fileIsOver)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                file >> city;
+                file >> street;
+                file >> house;
+                file >> kvartira;
+                Adress adress(city, street, house, kvartira);
+                arr[i] = adress.get_output_address();
+               // outFile << adress.get_output_address() << std::endl;
+
+            }
+
+            fileIsOver = file.eof();
+        }
+        sort_adress(arr, rows);
+        for (int i = 0; i < rows; i++)
+        {
+            outFile << arr[i] << std::endl;
+
+        }
+        outFile.close();
+    }
+    else
+    {
+        std::cout << "Не получилось открыть файл!" << std::endl;
+    }
+    file.close();
+
+    return 0;
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
