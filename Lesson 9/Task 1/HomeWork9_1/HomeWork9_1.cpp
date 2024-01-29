63ef08104de67519050c2aed09e9c3931dc53c61
Baryshkov Mikhail <m.baryshkov@mail.ru>
2024-01-29 21:23:35 +0300

    Готово задание 9-1

---
+++ Lesson 9/Task 1/HomeWork9_1/HomeWork9_1.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+
+class Fraction
+{
+private:
+	int numerator_;
+	int denominator_;
+
+public:
+	Fraction(int numerator, int denominator)
+	{
+		numerator_ = numerator;
+		denominator_ = denominator;
+	}
+
+	bool operator==(Fraction other) {
+		if ((numerator_ / denominator_) == (other.numerator_ / other.denominator_)) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+	}
+
+	bool operator!=(Fraction other) {
+		if ((numerator_ / denominator_) != (other.numerator_ / other.denominator_)) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+	}
+	bool operator>(Fraction other) {
+		if ((numerator_ / denominator_) > (other.numerator_ / other.denominator_)) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+	}
+	bool operator<(Fraction other) {
+		if ((numerator_ / denominator_) < (other.numerator_ / other.denominator_)) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+	}
+	bool operator>=(Fraction other) {
+		if ((numerator_ / denominator_) >= (other.numerator_ / other.denominator_)) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+	}
+	bool operator<=(Fraction other) {
+		if ((numerator_ / denominator_) <= (other.numerator_ / other.denominator_)) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+	}
+
+};
+
+int main()
+{
+	Fraction f1(4, 3);
+	Fraction f2(6, 11);
+
+	std::cout << "f1" << ((f1 == f2) ? " == " : " not == ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 != f2) ? " != " : " not != ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 < f2) ? " < " : " not < ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 > f2) ? " > " : " not > ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 <= f2) ? " <= " : " not <= ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 >= f2) ? " >= " : " not >= ") << "f2" << '\n';
+	return 0;
+}
