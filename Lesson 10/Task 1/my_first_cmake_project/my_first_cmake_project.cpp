5d64cb028e44bdf94978fc605677657983837791
Baryshkov Mikhail <m.baryshkov@mail.ru>
2024-02-01 23:02:38 +0300

    Готово задание 10-1

---
+++ Lesson 10/Task 1/my_first_cmake_project/my_first_cmake_project.cpp
@@ -0,0 +1,17 @@
+﻿#include <Windows.h>
+#include <string>
+#include <iostream>
+
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    std::string name;
+    std::cout << "Введите имя: ";
+    std::cin >> name;
+    std::cout << "\nЗдравствуйте, " << name << "!\n";
+
+    system("pause");
+    return 0;
+}
