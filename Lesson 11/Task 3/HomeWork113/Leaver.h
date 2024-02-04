50375c4037b2d4d13d0f240326c8ca650f0be14c
Baryshkov Mikhail <m.baryshkov@mail.ru>
2024-02-04 22:54:31 +0300

    Задание готово

--- Lesson 11/Task 3/HomeWork113/Leaver.h
+++ Lesson 11/Task 3/HomeWork113/Leaver.h
@@ -1,17 +1,19 @@
 #pragma once
 #include <string>
 
+/*
 #ifdef LEAVERDYNAMICLIBRARY_EXPORTS
 #define LEAVERDYNAMICLIB_API __declspec(dllexport)
 #else
 #define LEAVERDYNAMICLIB_API __declspec(dllimport)
 #endif
+*/
 
 namespace lib
 {
 	class Leaver
 	{
 	public:
-		LEAVERDYNAMICLIB_API std::string leave(std::string name);
+		 std::string leave(std::string name);
 	};
 }
