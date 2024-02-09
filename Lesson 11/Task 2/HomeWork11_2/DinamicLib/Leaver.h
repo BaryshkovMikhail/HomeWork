1e8eb68442ddc95c3678c075132e05a1fe3aad26
Baryshkov Mikhail <m.baryshkov@mail.ru>
2024-02-09 10:23:08 +0300

    Задание 11-2 исправлено

--- Lesson 11/Task 2/HomeWork11_2/DinamicLib/Leaver.h
+++ Lesson 11/Task 2/HomeWork11_2/DinamicLib/Leaver.h
@@ -1,17 +1,17 @@
 #pragma once
 #include <string>
 
-#ifdef LEAVERDYNAMICLIBRARY_EXPORTS
-#define LEAVERDYNAMICLIB_API __declspec(dllexport)
+#ifdef DINAMICLIB_EXPORTS
+#define DINAMICLIB_API __declspec(dllexport)
 #else
-#define LEAVERDYNAMICLIB_API __declspec(dllimport)
+#define DINAMICLIB_API __declspec(dllimport)
 #endif
 
 namespace lib
 {
-	class LEAVERDYNAMICLIB_API Leaver
+	class Leaver
 	{
 	public:
-		 std::string leave(std::string name);
+		DINAMICLIB_API std::string  leave(std::string name);
 	};
 }
